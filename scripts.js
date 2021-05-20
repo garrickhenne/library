2288eb01e7e8026e0cc66c89ec0d73d3afb2181d
Garrick Henne <garrickhenne@outlook.com>
2021-05-19 17:41:18 -0700

    Fully functioning, but lacking a interesting UI.

--- scripts.js
+++ scripts.js
@@ -15,11 +15,15 @@ Book.prototype.addBookToLibrary = function() {
     myLibrary.push(this);
 }
 
+Book.prototype.toggleRead = function() {
+    this.hasRead = !this.hasRead;
+}
+
 const harryPotter = new Book("JK Rowling", "Harry Potter: Prisoner of Azkaban", 300, true);
 const breakfast = new Book("Kurt Vonnegut", "Breakfast of Champions", 250, false);
 
-myLibrary.push(harryPotter);
-myLibrary.push(breakfast);
+harryPotter.addBookToLibrary();
+breakfast.addBookToLibrary();
 
 function renderBooks() {
     const container = document.querySelector('.container');
@@ -41,6 +45,21 @@ function renderBooks() {
         numPagesP.classList.add('card-writing');
         numPagesP.textContent = book.numPages;
 
+        const readP = document.createElement('p');
+        readP.classList.add('card-writing');
+        readP.textContent = book.hasRead ? "Read Already" : "Not read yet"; 
+
+        const readCheck = document.createElement('input');
+        readCheck.setAttribute('type', 'checkbox');
+        readCheck.checked = book.hasRead;
+        readCheck.addEventListener('change', (e) => {
+            let childArray = Array.from(e.path[1].childNodes);
+            let title = childArray[1].innerText;
+            let index = myLibrary.map((e) => e.title).indexOf(title);
+            myLibrary[index].toggleRead();
+            renderBooks();
+        });
+
         const deleteBtn = document.createElement('button');
         deleteBtn.classList.add('delete-btn');
         deleteBtn.textContent = "Remove";
@@ -54,6 +73,8 @@ function renderBooks() {
         bookCard.appendChild(authorP);
         bookCard.appendChild(titleP);
         bookCard.appendChild(numPagesP);
+        bookCard.appendChild(readP);
+        bookCard.appendChild(readCheck);
         bookCard.appendChild(deleteBtn);
 
         container.appendChild(bookCard);
@@ -62,6 +83,8 @@ function renderBooks() {
     }
 }
 
+
+
 function deleteBook(title) {
     let index = myLibrary.map((e) => e.title).indexOf(title);
     myLibrary.splice(index,1);
@@ -69,3 +92,30 @@ function deleteBook(title) {
 }
 
 renderBooks();
+
+const form = document.querySelector('.book-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newTitle = document.querySelector('#new-title');
+    const newAuthor = document.querySelector('#new-author');
+    const newPages = document.querySelector('#new-pages');
+    const newHasRead = document.querySelector('#has-read');
+
+    if (newTitle.value === '' || newAuthor.value === '' || newPages.value === '') {
+        console.log("You tried to add a new book but one of the values was empty...");
+        return;
+    }
+
+    let clientBook = new Book(newAuthor.value, newTitle.value, parseInt(newPages.value), newHasRead.checked);
+    clientBook.addBookToLibrary();
+    renderBooks();
+});
+
+
+// const submitBtn = document.querySelector('#submit-book-btn');
+// submitBtn.addEventListener('click', (e) => {
+//     console.log('yeah');
+
+
+//     renderBooks();
+// })
